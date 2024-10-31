cfacacd1cd04ebcde4e02a7431d5c5757190359a
Daria Milyakova <103563978+GithubAgitator@users.noreply.github.com>
2024-10-31 08:55:57 +0300

    task2

--- task_2/task2.py
+++ task_2/task2.py
@@ -2,3 +2,18 @@
 # имя, возраст, город и профессия. Напишите программу,
 # которая считывает файл и выводит только тех пользователей,
 # которые старше 30 лет и работают в указанной профессии.
+import json
+
+d = {}
+lst = []
+with open('task2.json', 'r', encoding='utf-8') as file:
+    f = json.load(file)
+    print(f)
+    for k in f:
+        if k['age'] > 30 and k['profession'] == 'программист':
+            lst.append(k)
+print(*lst)
+
+
+
+
