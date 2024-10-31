a392d649d450332ead29754b17bed412f103b320
Zhukovton <zhukovton@gmail.com>
2024-10-31 16:09:56 +0300

    Добавил 5 домашку

--- task_2/task2.py
+++ task_2/task2.py
@@ -2,3 +2,17 @@
 # имя, возраст, город и профессия. Напишите программу,
 # которая считывает файл и выводит только тех пользователей,
 # которые старше 30 лет и работают в указанной профессии.
+import json
+
+with open("task2.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+list_who_over_30 = []
+
+for person in data:
+    if "age" in person:
+        age = person.get("age")
+        if age >= 30:
+            list_who_over_30.append(person)
+
+print(list_who_over_30)
--- task_4/task4.py
+++ task_4/task4.py
@@ -3,3 +3,15 @@
 # Напишите программу, которая считывает данные
 # и выводит только тех сотрудников,
 # у которых зарплата больше 50,000.
+import csv
+
+with open("employees.csv", "r", encoding="utf-8") as file:
+    employees = list(csv.DictReader(file))
+
+list_who_earn_more_50k = []
+for employee in employees:
+    if "salary" in employee:
+        salary = int(employee.get("salary"))
+        if salary > 50000:
+            list_who_earn_more_50k.append(employee)
+print(list_who_earn_more_50k)
