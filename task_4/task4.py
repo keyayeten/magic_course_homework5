5146054e7376a251692206ca988a88545effa24a
Daria Milyakova <103563978+GithubAgitator@users.noreply.github.com>
2024-11-01 22:26:55 +0300

    task4

--- task_4/task4.py
+++ task_4/task4.py
@@ -3,3 +3,10 @@
 # Напишите программу, которая считывает данные
 # и выводит только тех сотрудников,
 # у которых зарплата больше 50,000.
+import csv
+
+with open('employees.csv', 'r', encoding='utf-8') as file:
+    f = csv.DictReader(file)
+    for i in f:
+        if int(i['salary']) > 50000:
+            print(i)
